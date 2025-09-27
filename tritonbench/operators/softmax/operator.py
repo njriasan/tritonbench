0938f4d0a6358bbe4eccc35ba07e216960c5ac27
Will Feng <yfeng.us@gmail.com>
2025-09-26 18:54:02 -0700

    softmax: change baseline to directly use F.softmax (#482)

--- tritonbench/operators/softmax/operator.py
+++ tritonbench/operators/softmax/operator.py
@@ -118,24 +118,10 @@ class Operator(BenchmarkOperator):
 
     @register_benchmark(baseline=True)
     def naive_softmax(self, x):
-        """Compute row-wise softmax of X using native pytorch
-        We subtract the maximum element in order to avoid overflows. Softmax is invariant to
-        this shift.
-        """
+        """Compute row-wise softmax of X using native pytorch."""
 
         def _inner():
-            # read  MN elements ; write M  elements
-            x_max = x.max(dim=1)[0]
-            # read MN + M elements ; write MN elements
-            z = x - x_max[:, None]
-            # read  MN elements ; write MN elements
-            numerator = torch.exp(z)
-            # read  MN elements ; write M  elements
-            denominator = numerator.sum(dim=1)
-            # read MN + M elements ; write MN elements
-            ret = numerator / denominator[:, None]
-            # in total: read 5MN + 2M elements ; wrote 3MN + 2M elements
-            return ret
+            return torch.nn.functional.softmax(x, dim=1)
 
         return _inner
 
