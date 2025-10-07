0bb2da4f7c9bf14a05bea70b18d093dbf299edb7
Xu Zhao <xzhao9@meta.com>
2025-10-07 17:01:23 -0400

    Migrate the grouped_gemm inputs from qwen3
    Differential Revision: D84006584
    
    Pull Request resolved: https://github.com/meta-pytorch/tritonbench/pull/522

--- tritonbench/data/loader.py
+++ tritonbench/data/loader.py
@@ -1,10 +1,11 @@
 import importlib
+import os
 from pathlib import Path
 from typing import Any
 
 from tritonbench.utils.env_utils import is_fbcode
 
-SUPPORTED_INPUT_OPS = ["highway_self_gating"]
+SUPPORTED_INPUT_OPS = ["highway_self_gating", "grouped_gemm"]
 
 INPUT_CONFIG_DIR = Path(__file__).parent.joinpath("input_configs")
 INTERNAL_INPUT_CONFIG_DIR = (
@@ -19,7 +20,15 @@ def get_input_loader(tritonbench_op: Any, op: str, input: str):
         hasattr(tritonbench_op, "aten_op_name") or op in SUPPORTED_INPUT_OPS
     ), f"Unsupported op: {op}. "
     op_module = ".".join(tritonbench_op.__module__.split(".")[:-1])
-    generator_module = importlib.import_module(f"{op_module}.input_loader")
-    input_iter_getter = generator_module.get_input_iter
-    input_iter = input_iter_getter(tritonbench_op, op, input)
-    return input_iter
+    generator_module = importlib.import_module(op_module)
+    input_loader_cls = generator_module.InputLoader
+    if os.path.exists(input):
+        input_file_path = Path(input)
+    elif INPUT_CONFIG_DIR.joinpath(input).exists():
+        input_file_path = INPUT_CONFIG_DIR.joinpath(input)
+    elif INTERNAL_INPUT_CONFIG_DIR.joinpath(input).exists():
+        input_file_path = INTERNAL_INPUT_CONFIG_DIR.joinpath(input)
+    else:
+        raise RuntimeError(f"Input file {input} does not exist.")
+    input_loader = input_loader_cls(tritonbench_op, op, input_file_path)
+    return input_loader.get_input_iter()
